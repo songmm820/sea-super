5f2da641b74136079235089eed4878781c9856e1
songmm <mmsong@yeah.net>
2025-01-18 21:07:18 +0800

    feat:项目初始化

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js
+export default {
+  semi: false, // 行尾分号
+  singleQuote: true, // 单引号
+  tabWidth: 2, // 缩进使用2个空格
+  trailingComma: 'none', //  最后一行不使用逗号
+  jsxSingleQuote: true // 在 JSX 中使用单引号而不是双引号
+}
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const host = process.env.TAURI_DEV_HOST
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors 防止 Vite 清除 Rust 显示的错误
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**']
+    }
+  },
+  // 3. // 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {}
+}))
