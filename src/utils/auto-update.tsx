3c417876e24f615e5de0479ffdb50cd8e0bdffc1
songmm <songmm@qingsongjituan.com>
2025-04-07 15:15:00 +0800

    feat：github action自动构建

--- src/utils/auto-update.tsx
+++ src/utils/auto-update.tsx
@@ -83,7 +83,7 @@ function AutoCheckUpdate() {
 
 /* eslint-disable no-console */
 // 打印当前开发环境
-console.log('当前环境：', import.meta.env.MODE)
+console.log('当前开发环境：', import.meta.env.MODE)
 
 // 开发环境不检测更新
 if (import.meta.env.MODE === 'production') {
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const host = process.env.TAURI_DEV_HOST
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   // 开发环境使用gitHub action部署到 github pages，所以需要配置base为仓库名
-  base: process.env.NODE_ENV === 'production' ? '/LetsPlay/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/LetsPlay' : '/',
   plugins: [
     react(),
     tailwindcss(),
