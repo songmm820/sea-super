bdf814db4aaac0ca22ad3cc8a500296239f9a78c
songmm <songmm@qingsongjituan.com>
2025-04-07 16:36:01 +0800

    feat：环境变量类型提示

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+// 环境变量类型声明
+interface ImportMetaEnv {
+  readonly VITE_SECOND_DEPLOY_DIR: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
