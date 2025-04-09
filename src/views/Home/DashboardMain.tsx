01273ed89fe93f62d947bd5b924754388f2eb017
songmm <mmsong@yeah.net>
2025-04-09 22:39:36 +0800

    feat：插件分组列表 @TODO 插件工具展示🔧

--- src/views/Home/DashboardMain.tsx
+++ src/views/Home/DashboardMain.tsx
@@ -7,14 +7,14 @@ import { CSSProperties } from 'react'
 import Groups from '@/views/PluginGroups/Groups.tsx'
 
 function DashboardMain() {
-  const styls: CSSProperties = {
+  const styles: CSSProperties = {
     backgroundImage: 'linear-gradient(0deg, #d5eaff, #fff)'
   }
 
   // 背景图区域
   const Banner = () => {
     return (
-      <div className='relative h-[260px]' style={styls}>
+      <div className='relative h-[260px]' style={styles}>
         <div className='absolute top-[100px] left-[50px] '></div>
       </div>
     )
